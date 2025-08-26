49b604a038d51bc92d2a8a09a2a5f24dbcbfc2c5
Jaimin Suthar <jaimin.suthar12@gmail.com>
2025-08-26 15:18:51 +0530

    New files added API call Update.

--- api/crypto.js
+++ api/crypto.js
@@ -17,11 +17,10 @@ export default async function handler(req, res) {
             return res.status(400).json({ error: "Text is required" });
         }
 
-        const encrypted = CryptoJS.AES.encrypt(text, secretKey).toString();
-        const bytes = CryptoJS.AES.decrypt(encrypted, secretKey);
+        const bytes = CryptoJS.AES.decrypt(text, secretKey);;
         const decrypted = bytes.toString(CryptoJS.enc.Utf8);
 
-        res.status(200).json({ encrypted, decrypted });
+        res.status(200).json({ decrypted });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Server Error", details: error.message });
